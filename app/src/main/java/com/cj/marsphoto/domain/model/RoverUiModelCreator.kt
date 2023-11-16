c55888068c4884cd785114d41c5bdbb4d7ed24f3
崔杰 <cuijie001024@qq.com>
2023-11-16 13:09:02 +0800

    create RoverList

---
+++ app/src/main/java/com/cj/marsphoto/domain/model/RoverUiModelCreator.kt
@@ -0,0 +1,8 @@
+package com.cj.marsphoto.domain.model
+
+import com.cj.marsphoto.R
+
+val roverUiModelList = listOf(
+    RoverUiModel("Cui Jie1", R.drawable.test_img_1, "2023-11-11 ", "1000km"),
+    RoverUiModel("Cui Jie2", R.drawable.test_img_2, "2023-10-10 ", "100km"),
+)
